7581000b4e5e5af10acf29613c12e6e8bb908259
Dennis Mullen <dkmullen@gmail.com>
2022-10-16 10:00:45 -0400

    Update text input for changing state

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, StyleSheet, View, TextInput } from 'react-native';
 
 const TextScreen = () => {
+  // name is set from parent (TextScreen) to child
+  // setName also comes from parent, is a callback to make changes
+  const [name, setName] = useState('');
   return (
     <View>
+      <Text>Name</Text>
       <TextInput
         style={styles.input}
         autoCapitalize='none'
         autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
       />
+      <Text>My name is {name}</Text>
+      {name && name.length < 5 ? (
+        <Text>Entry must be at least 5 characters</Text>
+      ) : null}
     </View>
   );
 };
